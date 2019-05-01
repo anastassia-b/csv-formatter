1451a19beec20a142b130466dff26637af7c41e6
Anastassia Bobokalonova <email.anastassia@gmail.com>
2019-05-01 00:28:00 -0700

    Remove minimist

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-var fs = require("fs");
+const fs = require("fs");
 const Papa = require("papaparse");
-var stringify = require("csv-stringify");
+const args = require("minimist")(process.argv.slice(2));
 
-fs.readFile("./input.csv", "utf8", function(err, contents) {
-  Papa.parse(contents, {
-    complete: function(results) {
-      console.log("Finished parsing CSV to JSON!");
-      const data = results.data;
-      replaceEmptyValues(data);
-      extendObjectivesDown(data);
-      const data2 = removeRowsRoadmapItems(data);
-      const data3 = keepRowsKeyResults(data2);
-      const data4 = removeTemplateRows(data3);
-      saveNewFile(data4);
-    }
-  });
-});
+main();
 
-function saveNewFile(data) {
-  stringify(data, function(err, output) {
-    fs.writeFile("output.csv", output, "utf8", function(err) {
-      if (err) {
-        console.log(`Error occured saving the file.\n${err}`);
-      } else {
-        console.log("New file saved!");
+function main() {
+  const inputFileName = args["i"] || args["input"];
+  const outputFileName = args["o"] || args["output"] || "output.csv";
+  if (!inputFileName) {
+    console.log("Please specify an input file with --input=your-filename.csv.");
+    return;
+  }
+  fs.readFile(inputFileName, "utf8", function(err, contents) {
+    Papa.parse(contents, {
+      complete: function(results) {
+        console.log("Finished parsing CSV to JSON!");
+        const data = results.data;
+        replaceEmptyValues(data);
+        extendObjectivesDown(data);
+        const data2 = removeRowsRoadmapItems(data);
+        const data3 = keepRowsKeyResults(data2);
+        const data4 = removeTemplateRows(data3);
+        saveNewFile(data4, outputFileName);
       }
     });
+  });
+}
+
+function saveNewFile(data, outputFileName) {
+  const output = Papa.unparse(data);
+  fs.writeFile(outputFileName, output, "utf8", function(err) {
+    if (err) {
+      console.log(`Error occured saving the file.\n${err}`);
+    } else {
+      console.log(`New file saved: ${outputFileName}!`);
+    }
   });
 }
 
